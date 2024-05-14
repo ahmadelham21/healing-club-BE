1e67ff8db1a95f83fb57f242e307ca8811829a0d
Ahmad Ilham <ahmadelham21@gmail.com>
2024-05-14 08:27:07 +0700

    INITIAL COMMIT

---
+++ src/test/java/com/example/healingclub/HealingClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.healingclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealingClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
