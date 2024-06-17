acb84d815a6b8b17585b382a67e6af19a78d309c
Ahmad Ilham <ahmadelham21@gmail.com>
2024-06-18 00:22:19 +0700

    [ADD] adding ErrorController,ErrorMessage,ResponseMessage

---
+++ src/main/java/com/example/healingclub/constant/ErrorMassage.java
@@ -0,0 +1,5 @@
+package com.example.healingclub.constant;
+
+public class ErrorMassage {
+    public final static String INTERNAL_SERVER_ERROR = "internal server error";
+}
--- src/main/java/com/example/healingclub/constant/ResponseMessage.java
+++ src/main/java/com/example/healingclub/constant/ResponseMessage.java
@@ -1,9 +1,24 @@
 package com.example.healingclub.constant;
 
 public class ResponseMessage {
+    //Response Message for facility api
     public final static String CREATED_FACILITY = "Successfully create new facility";
     public final static String GET_FACILITY = "Successfully get facility";
     public final static String UPDATE_FACILITY = "Successfully update facility";
     public final static String DELETE_FACILITY = "Successfully delete facility";
+
+    //Response Message for hotel api
+    public final static String CREATED_HOTEL = "Successfully create new hotel";
+    public final static String GET_HOTEL = "Successfully get hotel";
+    public final static String UPDATE_HOTEL = "Successfully update hotel";
+    public final static String DELETE_HOTEL = "Successfully delete hotel";
+
+    //Response Message for city api
+    public final static String CREATED_CITY = "Successfully create new city";
+    public final static String GET_CITY = "Successfully get city";
+    public final static String UPDATE_CITY = "Successfully update city";
+    public final static String DELETE_CITY = "Successfully delete city";
+
+
 
 }
