ccf98514d98db5a8140cd14488a9c62edadbc71f
Ahmad Ilham <ahmadelham21@gmail.com>
2024-06-18 00:27:31 +0700

    [ADD]adding  ErrorMessage.NOT_FOUND

--- src/main/java/com/example/healingclub/constant/ErrorMassage.java
+++ src/main/java/com/example/healingclub/constant/ErrorMassage.java
@@ -2,4 +2,6 @@ package com.example.healingclub.constant;
 
 public class ErrorMassage {
     public final static String INTERNAL_SERVER_ERROR = "internal server error";
+    public final static String NOT_FOUND = "Data not found";
+
 }
